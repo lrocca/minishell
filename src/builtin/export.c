882f70ac1b5c4e88796850368cb4a5ec9278e44c
Leonardo Rocca <lrocca@student.42roma.it>
2021-07-06 16:12:27 +0200

    export: print env in ascending order

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 19:08:55 by lrocca            #+#    #+#             */
-/*   Updated: 2021/07/06 03:09:46 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/07/06 16:12:22 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,48 +18,29 @@ static void	print_var(char *var)
 	ft_putendl_fd(var, STDOUT_FILENO);
 }
 
-/* static char	print_env(void)
+static char	print_env(void)
 {
 	t_list	*min;
 	t_list	*curr;
 	t_list	*prev;
-	t_list	*first;
 	int		len;
 
-	len = ft_lstsize(g_ms.env) - 1;
-	first = g_ms.env;
-	curr = g_ms.env;
-	while (curr)
-	{
-		if (ft_strcmp(curr->content, first->content) < 0)
-			first = curr;
-		curr = curr->next;
-	}
-	print_var(first->content);
-	prev = first;
-	// min = prev;
+	prev = NULL;
+	len = ft_lstsize(g_ms.env);
 	while (len--)
 	{
 		curr = g_ms.env;
-		min = g_ms.env;
-		while (min == prev)
-			min = min->next;
+		min = NULL;
 		while (curr)
 		{
-			if (ft_strcmp(curr->content, min->content) < 0 \
-				&& ft_strcmp(curr->content, prev->content) > 0)
+			if ((!min || ft_strcmp(curr->content, min->content) < 0) \
+				&& (!prev || ft_strcmp(curr->content, prev->content) > 0))
 				min = curr;
 			curr = curr->next;
 		}
 		print_var(min->content);
 		prev = min;
 	}
-	return (0);
-} */
-
-static char	print_env(void)
-{
-	print_var(ft_getenv("USER"));
 	return (0);
 }
 
