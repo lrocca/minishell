606812af87301ea597b8a905e484e562c96c9700
leeoocca <leeoocca@gmail.com>
2021-07-06 02:33:58 +0200

    exit: check numeric argument

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 19:09:18 by lrocca            #+#    #+#             */
-/*   Updated: 2021/07/05 20:04:28 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/07/06 02:33:29 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 char	builtin_exit(t_list *av)
 {
-	if (av && av->next)
-		g_ms.status = ft_atoi(av->next->content); // TODO check numeric argument
+	if (av && av->next && ft_isdigit(((char *)av->next->content)[0]))
+		g_ms.status = ft_atoi(av->next->content);
 	ft_putendl_fd("exit", STDOUT_FILENO);
 	ft_lstclear(&g_ms.env, free);
 	ft_paths(NULL, PATHS_FREE);
-	exit(g_ms.status); //  should actually wait for other cmds
+	exit(g_ms.status);
 	return (0);
 }
