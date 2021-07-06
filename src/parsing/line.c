9e062c57571a4efc1cbb2ca4907912058535cc0a
Leonardo Rocca <lrocca@student.42roma.it>
2021-07-06 13:41:29 +0200

    line: support for empty arguments

--- src/parsing/line.c
+++ src/parsing/line.c
@@ -6,20 +6,25 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/03 03:40:49 by lrocca            #+#    #+#             */
-/*   Updated: 2021/07/06 03:05:27 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/07/06 13:39:15 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	handle_quotes(char **buff, char *quote, char new)
+static char	handle_quotes(char **buff, char *quote, char new, char next)
 {
 	if (!*quote)
 		*quote = new;
 	else if (*quote == new)
+	{
 		*quote = 0;
+		if (!*buff && (!next || ft_ismeta(next) || ft_isspace(next)))
+			return (1);
+	}
 	else
 		char_to_buff(buff, new);
+	return (0);
 }
 
 static char	skip_spaces(const char *line, int *i)
@@ -47,7 +52,10 @@ char	*line_to_word(const char *line, int *i)
 		if (!quote && (ft_ismeta(line[*i]) || (buff && ft_isspace(line[*i]))))
 			break ;
 		else if (line[*i] == '\'' || line[*i] == '\"')
-			handle_quotes(&buff, &quote, line[*i]);
+		{
+			if (handle_quotes(&buff, &quote, line[*i], line[*i + 1]))
+				return (ft_strdup(""));
+		}
 		else if (line[*i] == '$' && quote != '\'')
 			handle_dollar(&buff, line, i);
 		else
