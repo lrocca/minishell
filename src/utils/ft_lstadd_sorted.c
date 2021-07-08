33e7b43e8deb0fc32479a57eb56d6a4285184ecd
Leonardo Rocca <lrocca@student.42roma.it>
2021-07-08 18:32:39 +0200

    wildcard: fix sorting algo

--- src/utils/ft_lstadd_sorted.c
+++ src/utils/ft_lstadd_sorted.c
@@ -6,7 +6,7 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 08:01:43 by lrocca            #+#    #+#             */
-/*   Updated: 2021/07/08 08:12:12 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/07/08 18:31:19 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	ft_lstadd_sorted(t_list **head, t_list *new)
 		new->next = curr;
 		return ;
 	}
-	while (curr->next && ft_strcmp(curr->content, new->content) < 0)
+	while (curr->next && ft_strcmp(curr->next->content, new->content) < 0)
 		curr = curr->next;
 	new->next = curr->next;
 	curr->next = new;
